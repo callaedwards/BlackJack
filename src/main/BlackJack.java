3443e28b8f72ab1e421fb3c89c89b535240909f7
Calla Edwards <callaed98@gmail.com>
2017-05-24 13:16:14 -0400

    refactored main function

--- src/main/BlackJack.java
+++ src/main/BlackJack.java
@@ -1,90 +1,131 @@
 import java.util.*;
+
 public class BlackJack
 {
-    public static void main (String [] args)
+	public static void main (String [] args)
     {
-        Scanner console = new Scanner(System.in);
-        boolean play = true;
-	String n = "";
-
-	while (play)
-	{	
-		System.out.println("Welcome to Blackjack! \nWant to play? (Y / N)");
-        	n = console.nextLine();
-	
-		if (n.equals("Y") || n.equals("y"))
-			play=false;
-		else if (n.equals("N") || n.equals("n"))
-		{
-			System.out.println("Goodbye");
-			play=false;
-		}
-		else
-		{
-			System.out.println("Invalid Answer. Try again.");
-			play=true;
-		}
+    	new BlackJack().play();
 	}
 
-	int a= (int)(Math.random()*13) + 1;
-	int b= (int)(Math.random()*13) + 1;
-	int c= (int)(Math.random()*13) + 1;
-	int d= (int)(Math.random()*13) + 1;
-	int e=0;
+   private void play()
+   {
+    	boolean keepPlaying = true;
 
-	System.out.printf("Your cards are %s and %s. The dealer's card are %s and %s.",faceValue(a),faceValue(b),faceValue(c),faceValue(d));
-	int yourCards = cardValue(a)+cardValue(b);
-	int myCards = cardValue(c)+cardValue(d);
-	play=true;
-	
-	while(play)
-	{
-		System.out.println(" Hit or Stand?");
-		n = console.nextLine();
+		System.out.println("Welcome to Blackjack!");
+	    
+	    while (keepPlaying)
+	    {
+			int a = deal();
+			int b = deal();
+			int c = deal();
+			int d = deal();
 
-		if (n.equals("Hit") || n.equals("hit"))
-		{
-			e= 1 + (int)(Math.random()*13);
-			System.out.printf("Your card is %s.",faceValue(e));
-			yourCards += cardValue(e);
-			play=true;
-		}
-		else if (n.equals("Stand") || n.equals("stand"))
-			play=false; 
-		else
-		{
-			System.out.println("Invalid Answer. Try Again.");
-			play=true;
-		}
-	}
-	play=true;
-	while (play)
-	{
-		if (myCards < 16)
-		{
-			e = 1 + (int)(Math.random()*13);
-			System.out.printf("Dealer Hits. Dealer draws a %s. ",faceValue(e));
-			myCards += cardValue(e);
-			play=true;
-		}
-		if (myCards > 21)
-			System.out.print("Dealer Busted!");
-		else
-		{
-			System.out.println("Dealer Stands");
-			play=false;
-		}
-	}
-	
-	if (yourCards > 21)
-		System.out.println("You busted! Dealer Wins!");
-	else if (myCards > 21 || yourCards > myCards)
-		System.out.println("You Win!");
-	else if (myCards > yourCards)
-		System.out.println("Dealer Wins!");
-	else
-		System.out.println("It's a tie!");
+			System.out.printf("Your cards are %s and %s. The dealer's card are %s and %s.",faceValue(a),faceValue(b),faceValue(c),faceValue(d));
+			int playerCards = cardValue(a) + cardValue(b);
+			int dealerCards = cardValue(c) + cardValue(d);
+			
+			playerCards = computePlayerCards(playerCards);
+			dealerCards = computeDealerCards(dealerCards);
+			
+			winner(playerCards, dealerCards);
+			keepPlaying = playAgain();
+		}   	
    }
+
+   private static boolean playAgain()
+   {
+   		Scanner console = new Scanner(System.in);
+		boolean play = true;
+		boolean invalidAnswer = true;
+
+		while (invalidAnswer)
+		{
+			System.out.println("Do you want to play again? (Y/N)");
+	   		String n = console.nextLine();
+
+	   		if (n.equalsIgnoreCase("y"))
+	   		{
+	   			play = true;
+	   			invalidAnswer = false;
+	   		}
+	   		else if (n.equalsIgnoreCase("n"))
+	   		{
+	   			play = false;
+	   			invalidAnswer = false;
+	   		}
+	   	}
+
+	   	return play;
+   	}
+
+
+   private static void winner(int playerCards, int dealerCards)
+   {
+   		if (playerCards > 21)
+			System.out.println("You busted! Dealer Wins!");
+		else if (dealerCards > 21 || playerCards > dealerCards)
+			System.out.println("You Win!");
+		else // (dealerCards >= playerCards)
+			System.out.println("Dealer Wins!");
+   }
+
+   private static int deal()
+   {
+   		return (int)(Math.random()*13) + 1;
+   }
+   	
+   	private static int computeDealerCards(int dealerCards)
+   	{
+		boolean play = true;
+
+		while (play)
+		{
+			if (dealerCards < 16)
+			{
+				int e = 1 + (int)(Math.random()*13);
+				System.out.printf("Dealer Hits. Dealer draws a %s. ",faceValue(e));
+				dealerCards += cardValue(e);
+				play=true;
+			}
+			if (dealerCards > 21)
+				System.out.print("Dealer Busted!");
+			else
+			{
+				System.out.println("Dealer Stands");
+				play=false;
+			}
+		}
+		return dealerCards;
+   	}
+   	private static int computePlayerCards(int playerCards)
+   	{
+   		boolean play = true;
+        Scanner console = new Scanner(System.in);
+
+		while(play)
+		{
+			System.out.println(" Hit or Stand? (h/s)");
+			String n = console.nextLine();
+
+			if (n.equalsIgnoreCase("H"))
+			{
+				int e= 1 + (int)(Math.random()*13);
+				System.out.printf("Your card is %s.",faceValue(e));
+				playerCards += cardValue(e);
+				play=true;
+			}
+			else if (n.equalsIgnoreCase("S"))
+				play=false; 
+			else
+			{
+				System.out.println("Invalid Answer. Try Again.");
+				play=true;
+			}
+		}
+
+		return playerCards;
+	}
+   
    public static String faceValue(int n)
    {
    	if (n==1)
@@ -98,11 +139,12 @@ public class BlackJack
 	else // (n==13)
 		return "K";
    }
+   
    public static int cardValue(int n)
    {
-   	if (n<=10)
-	return n;
-	else
-	return 10;
+   		if (n<=10)
+			return n;
+		else
+			return 10;
    }
 }
